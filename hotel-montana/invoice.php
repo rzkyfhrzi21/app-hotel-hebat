cfdcd0f1a1d62618557e2a3f507772e1ec2bb9a1
rzkyfhrzi21 <rzkyfhrzi21@gmail.com>
2023-03-01 11:09:05 +0700

    11;09

--- hotel-montana/invoice.php
+++ hotel-montana/invoice.php
@@ -1,99 +1,102 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<?php
 
-<body style="padding: 0 20;">
-    <div>
+include 'koneksi.php';
 
-        <section class="content">
-            <div class="row">
-                <div>
-                    <div class="span12">
-                        <table class="table">
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <h2><strong>No Tagihan </strong># </h2>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+$no = 1;
+
+date_default_timezone_set('Asia/Jakarta');
+
+$tanggal = date('l, d M Y');
+
+$query = mysqli_query($koneksi, "SELECT * FROM pemesanan order by id_pemesanan desc limit 1");
+
+while ($row = mysqli_fetch_array($query)) {
+
+?>
+
+    <!-- Theme style -->
+    <link rel="stylesheet" href="template1/css/bootstrap.min.css">
+
+    <body style="padding: 0 20;">
+        <div>
+            <section class="content">
+                <div class="row">
+                    <div>
+                        <div class="span12">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <h2><strong>No Tagihan </strong>#<?php echo $row['id_pemesanan'] ?></h2>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-4">
-                    From
-                    <address>
-                        <strong>Admin Sahretech</strong><br>
-                        Jl. Sudirman No.3012, Palembang<br>
-                        Kec. Palembang Raya, Palembang,<br>
-                        Sumatera selatan 30961<br>
-                        Phone: (804) 123-5432<br>
-                        Email: info@sahretech.com
-                    </address>
+                <div class="row">
+                    <div class="col-sm-4">
+                        From
+                        <address>
+                            <strong>Admin Hotel Hebat</strong><br>
+                            Jl. Swadhipa No. 217, Bumisari<br>
+                            Kec. Natar, Lampung Selatan,<br>
+                            Lampung 35362<br>
+                            Phone: (62) 851-7320-0421<br>
+                            Email: admin@hotelhebat.com
+                        </address>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-4">
+                        To
+                        <address>
+                            <strong>
+                                <td> <?php echo $row['nama_pemesan'] ?> </td>
+                            </strong><br>
+                            <?php echo $tanggal; ?>,<br>
+                            Lampung 35362,<br>
+                            Phone: <td> <?php echo $row['no_hp'] ?>, </td><br>
+                            Email: <?php echo $row['email'] ?>
+                        </address>
+                    </div>
                 </div>
-                <!-- /.col -->
-                <div class="col-sm-4">
-                    To
-                    <address>
-                        <strong></strong><br>
-                        Jl. Sudirman No. 3012, Palembang<br>
-                        Kec. Palembang Raya, Palembang,<br>
-                        Sumatera selatan 30961<br>
-                        Phone: (555) 539-1037<br>
-                        Email: nbelputra437@gmail.com
-                    </address>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Nama Pemesan</th>
-                                <th>Nomor Hp</th>
-                                <th>Email</th>
-                                <th>Nama Tamu</th>
-                                <th>Check In</th>
-                                <th>Check Out</th>
-                                <th>Tipe Kamar</th>
-                                <th>Jumlah Kamar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-
-                            include 'koneksi.php';
-
-                            $no = 1;
-
-                            $query = mysqli_query($koneksi, "SELECT * FROM pemesanan order by id_pemesanan desc limit 1");
-
-                            while ($row = mysqli_fetch_array($query)) {
-                            ?>
+                <div class="row">
+                    <div class="col-xs-12 table-responsive">
+                        <table class="table table-striped">
+                            <thead>
                                 <tr>
-                                    <td> <?php echo $no++ ?> </td>
-                                    <td> <?php echo $row['nama_pemesan'] ?> </td>
-                                    <td> <?php echo $row['no_hp'] ?> </td>
-                                    <td> <?php echo $row['email'] ?> </td>
+                                    <th class="text-center">No</th>
+                                    <th>Nama Tamu</th>
+                                    <th>Tanggal Check In</th>
+                                    <th>Tanggal Check Out</th>
+                                    <th>Tipe Kamar</th>
+                                    <th>Jumlah Kamar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="text-center"> <?php echo $no++ ?> </td>
                                     <td> <?php echo $row['nama_tamu'] ?> </td>
                                     <td> <?php echo $row['check_in'] ?> </td>
                                     <td> <?php echo $row['check_out'] ?> </td>
                                     <td> <?php echo $row['tipe_kamar'] ?> </td>
                                     <td> <?php echo $row['jumlah_kamar'] ?> </td>
                                 </tr>
-                            <?php } ?>
-                            <tr>
-                                <td colspan="3"></td>
-                                <td><b>Total Biaya</b></td>
-                                <td><b></b></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-        </section>
-    </div>
-</body>
-<script>
-    window.print()
-</script>
+                                <tr>
+                                    <td colspan="4"></td>
+                                    <td><b>Total Kamar</b></td>
+                                    <td><b> <?php echo $row['jumlah_kamar'] ?> </b></td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
+            </section>
+        </div>
+        <script>
+            window.print()
+        </script>
+    </body>
+
+<?php } ?>
--- hotel-montana/simpan.php
+++ hotel-montana/simpan.php
@@ -1,9 +1,7 @@
 <?php
 
-// PANGGIL FILE KONEKSI.PHP
 include 'koneksi.php';
 
-// TAMPUNG NILAI DARI FIELD DI FORM PESAN KAMAR KEDALAM VARIABEL
 $nama_pemesan   = $_POST['nama_pemesan'];
 $email          = $_POST['email'];
 $no_hp          = $_POST['no_hp'];
@@ -13,21 +11,19 @@ $check_out      = $_POST['check_out'];
 $jumlah_kamar   = $_POST['jumlah_kamar'];
 $tipe_kamar     = $_POST['tipe_kamar'];
 
-// QUERY TAMBAH PESAN KAMAR KE TABEL PEMESANAN
 $query          = "INSERT INTO pemesanan SET nama_pemesan = '$nama_pemesan', email = '$email', no_hp = '$no_hp', nama_tamu ='$nama_tamu', check_in = '$check_in', check_out = '$check_out', jumlah_kamar = ' $jumlah_kamar', tipe_kamar = '$tipe_kamar'";
 $pesan_kamar    = mysqli_query($koneksi, $query);
 
-// JIKA PESAN KAMAR BERHASIL
 if ($pesan_kamar) {
     echo "<script>
-            window.open('invoice.php');       // MEMBUKA HALAMAN BARU BUKTI PEMESANAN
-            location.replace('kamar.php');  // MEREFRESH HALAMAN KE KAMAR.PHP
+            window.open('invoice.php');      
+            location.replace('kamar.php');  
         </script>";
 }
 // JIKA PESAN KAMAR GAGAL 
 else {
     echo "<script>
-            alert('Kamar gagal dipesan!');  // ALERT JIKA PESAN KAMAR GAGAL
-            location.replace('kamar.php');  // MEREFRESH HALAMAN KE KAMAR.PHP
+            alert('Kamar gagal dipesan!'); 
+            location.replace('kamar.php');  
         </script>";
 }
